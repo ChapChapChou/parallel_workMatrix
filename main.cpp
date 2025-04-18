777fc381647c7ea55b0fb01a6e34b70b1c23d032
ChapChapChou <1092533908@qq.com>
2025-04-17 23:24:48 -0500

    project update

---
+++ main.cpp
@@ -0,0 +1,147 @@
+/* main.cpp */
+
+//
+// Parallelizes a generic "work matrix" where work is randomly
+// distributed in an NxN matrix. Naive parallelization works,
+// but doesn't scale. A much more dynamic solution is needed.
+// 
+// Usage:
+//   work [-?] [-t NumThreads]
+//
+// Author:
+//   Zhou Hang
+//   Northwestern University
+// 
+// Initial template:
+//   Prof. Joe Hummel
+//   Northwestern University
+//
+
+#include <iostream>
+#include <string>
+#include <cstring>
+#include <chrono>
+#include <sys/sysinfo.h>
+#include <omp.h>
+
+#include "alloc2D.h"
+#include "workmatrix.h"
+
+using namespace std;
+
+
+//
+// Globals:
+//
+static int _numThreads = 1;  // default to sequential execution
+static int cells = 0;
+
+//
+// Function prototypes:
+//
+static void ProcessCmdLineArgs(int argc, char* argv[]);
+
+
+//
+// main:
+//
+int main(int argc, char *argv[])
+{
+	cout << "** Work Matrix Application **" << endl;
+	cout << endl;
+
+	//
+	// Set defaults, process environment & cmd-line args:
+	//
+	ProcessCmdLineArgs(argc, argv);
+
+	WorkMatrix wm;  // NOTE: wm MUST be created in sequential code.
+
+	cout << "Matrix size:  " << wm.num_rows() << "x" << wm.num_cols() << endl;
+	cout << "# of threads: " << _numThreads << " / " <<  omp_get_num_procs() << endl;
+	cout << endl;
+
+	cout << "working";
+	cout.flush();
+
+	//
+	// Solve each cell in the work matrix. Compute time for speedup
+	// calculations.
+	//
+  auto start = chrono::high_resolution_clock::now();
+
+  // parallel clause
+  #pragma omp parallel num_threads(_numThreads) default(none) shared(wm, cells, cout, _numThreads) 
+  //wm, cells and cout _numThreads is defined outside the parallel region
+  {
+	// set the number of threads for the current thread
+	omp_set_num_threads(_numThreads);
+  	#pragma omp for schedule(dynamic)
+	for (int r = 0; r < wm.num_rows(); r++) {
+		for (int c = 0; c < wm.num_cols(); c++) {
+
+			//
+			// this solves the work in cell [r][c]:
+			//
+			wm.do_work(r, c);
+
+			//
+			// show some output every 100 cells so we see progress:
+			//
+			#pragma omp atomic	// increment the cell count atomically
+			cells++;
+			
+			#pragma omp critical	// only on tread can print at a time
+			{
+				if (cells % 100 == 0) {
+					cout << ".";
+					cout.flush();
+				}
+			}
+		}
+	}
+  } // end of parallel region
+  
+  auto stop = chrono::high_resolution_clock::now();
+  auto diff = stop - start;
+  auto duration = chrono::duration_cast<chrono::milliseconds>(diff);
+
+	cout << endl;
+	cout << endl;
+
+  cout << endl;
+  cout << "** Done!  Time: " << duration.count() / 1000.0 << " secs" << endl;
+	cout << "** Execution complete **" << endl;
+  cout << endl;
+
+	return 0;
+}
+
+
+//
+// processCmdLineArgs:
+//
+static void ProcessCmdLineArgs(int argc, char* argv[])
+{
+	for (int i = 1; i < argc; i++)
+	{
+
+		if (strcmp(argv[i], "-?") == 0)  // help:
+		{
+			cout << "**Usage: work [-?] [-t NumThreads]" << endl << endl;
+			exit(0);
+		}
+		else if ((strcmp(argv[i], "-t") == 0) && (i+1 < argc))  // # of threads:
+		{
+			i++;
+			_numThreads = atoi(argv[i]);
+		}
+		else  // error: unknown arg
+		{
+			cout << "**Unknown argument: '" << argv[i] << "'" << endl;
+			cout << "**Usage: work [-?] [-t NumThreads]" << endl << endl;
+			exit(0);
+		}
+
+	}//for
+}
